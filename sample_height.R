b10954e6864c726583bdba61099925568845ff77
Simon König <simonkoenig@live.de>
2021-06-17 21:30:50 +0200

    add crown area estimation

--- sample_height.R
+++ sample_height.R
@@ -15,68 +15,91 @@ trees
 
 ## Große Ohe
 go_catch <- st_read("GO_catch.shp") %>% 
-  st_intersection(trees) %>% 
+  st_intersection(trees) %>%
+  mutate(Crown_Area = st_area(geometry)) %>% 
   st_drop_geometry() %>%
   as_tibble() %>% 
   mutate(Type = "go_catch") %>% 
   write_csv(
     "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/extracted/go_catch.csv")
 
+Sys.time()
+
 go_stream <- st_read("GO_StreamB50.shp") %>% 
   st_intersection(trees) %>% 
+  mutate(Crown_Area = st_area(geometry)) %>% 
   st_drop_geometry()%>% 
   as_tibble() %>% 
   mutate(Type = "go_stream") %>% 
   write_csv(
     "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/extracted/go_stream.csv")
 
+Sys.time()
+
 go_100 <- st_read("GO_100mB50.shp") %>% 
   st_intersection(trees) %>% 
-  st_drop_geometry()%>% 
+  mutate(Crown_Area = st_area(geometry)) %>% 
+  st_drop_geometry() %>% 
   as_tibble() %>% 
   mutate(Type = "go_100") %>% 
   write_csv(
     "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/extracted/go_100.csv")
 
+Sys.time()
+
 go_500 <- st_read("GO_500mB50.shp") %>% 
-  st_intersection(trees) %>% 
+  st_intersection(trees) %>%
+  mutate(Crown_Area = st_area(geometry)) %>% 
   st_drop_geometry()%>% 
   as_tibble() %>% 
   mutate(Type = "go_500") %>% 
   write_csv(
     "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/extracted/go_500.csv")
 
+Sys.time()
+
 ## Vydra
 vy_catch <- st_read("VY_catch.shp") %>% 
   st_intersection(trees) %>% 
+  mutate(Crown_Area = st_area(geometry)) %>% 
   st_drop_geometry()%>% 
   mutate(Type = "vy_catch") %>% 
   write_csv(
     "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/extracted/vy_catch.csv")
 
-# use already clipped file because it takes too long otherwise
-vy_stream <- st_read("vy_stream.gpkg") %>% 
-  st_drop_geometry()%>% 
-  as_tibble() %>% 
-  mutate(Type = "vy_stream") %>% 
-  write_csv(
-    "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/extracted/vy_stream.csv")
+Sys.time()
 
 vy_100 <- st_read("VY_100mB50.shp") %>% 
   st_intersection(trees) %>% 
+  mutate(Crown_Area = st_area(geometry)) %>% 
   st_drop_geometry()%>%
   as_tibble() %>% 
   mutate(Type = "vy_100") %>% 
   write_csv(
     "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/extracted/vy_100.csv")
 
+Sys.time()
+
 vy_500 <- st_read("VY_500mB50.shp") %>% 
-  st_intersection(trees) %>% 
+  st_intersection(trees) %>%
+  mutate(Crown_Area = st_area(geometry)) %>% 
   st_drop_geometry()%>% 
   as_tibble() %>% 
   mutate(Type = "vy_500") %>% 
   write_csv(
     "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/extracted/vy_500.csv")
+
+Sys.time()
+
+# use already clipped file because it takes too long otherwise
+vy_stream <- st_read("vy_stream.gpkg") %>% 
+  st_intersection(trees) %>%
+  mutate(Crown_Area = st_area(geometry)) %>% 
+  st_drop_geometry()%>% 
+  as_tibble() %>% 
+  mutate(Type = "vy_stream") %>% 
+  write_csv(
+    "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/extracted/vy_stream.csv")
 
 
 # max_catch <- go_catch %>% 
