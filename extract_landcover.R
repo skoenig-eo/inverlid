6910356152f6acbeefbd0e72f12c56773c36b1e0
Simon König <simonkoenig@live.de>
2021-06-23 09:31:12 +0200

    implement data wrangling for efficient results

--- extract_landcover.R
+++ extract_landcover.R
@@ -17,41 +17,97 @@ make_table <- function(i){
 
 go_100 <- raster("D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/rasters/s2lc_reprojected.tif") %>% 
   extract(st_read("GO_100mB50.shp")) %>% 
-  lapply(make_table)
+  lapply(make_table) %>% 
+  bind_rows(.id = "column_label") %>% 
+  dplyr::select(-column_label) %>% 
+  mutate(Pixels = rowSums(across(where(is.numeric)), na.rm = TRUE)) %>% 
+  mutate_at(vars(-Pixels), funs(./Pixels)) %>% 
+  bind_cols(sizes_go %>% dplyr::select(LocCode)) %>% 
+  mutate(River = "GO",
+         Class = "100")
 
 go_500 <- raster("D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/rasters/s2lc_reprojected.tif") %>% 
   extract(st_read("GO_500mB50.shp")) %>% 
-  lapply(make_table)
+  lapply(make_table) %>% 
+  bind_rows(.id = "column_label")%>% 
+  dplyr::select(-column_label) %>% 
+  mutate(Pixels = rowSums(across(where(is.numeric)), na.rm = TRUE)) %>% 
+  mutate_at(vars(-Pixels), funs(./Pixels)) %>% 
+  bind_cols(sizes_go %>% dplyr::select(LocCode)) %>% 
+  mutate(River = "GO",
+         Class = "500")
 
 go_stream <- raster("D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/rasters/s2lc_reprojected.tif") %>% 
   extract(st_read("GO_StreamB50.shp")) %>% 
-  lapply(make_table)
+  lapply(make_table) %>% 
+  bind_rows(.id = "column_label")%>% 
+  dplyr::select(-column_label) %>% 
+  mutate(Pixels = rowSums(across(where(is.numeric)), na.rm = TRUE)) %>% 
+  mutate_at(vars(-Pixels), funs(./Pixels)) %>% 
+  bind_cols(sizes_go %>% dplyr::select(LocCode)) %>% 
+  mutate(River = "GO",
+         Class = "Stream")
 
 go_catch <- raster("D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/rasters/s2lc_reprojected.tif") %>% 
   extract(st_read("GO_catch.shp")) %>% 
-  lapply(make_table)
+  lapply(make_table) %>% 
+  bind_rows(.id = "column_label")%>% 
+  dplyr::select(-column_label) %>% 
+  mutate(Pixels = rowSums(across(where(is.numeric)), na.rm = TRUE)) %>% 
+  mutate_at(vars(-Pixels), funs(./Pixels)) %>% 
+  bind_cols(sizes_go %>% dplyr::select(LocCode)) %>% 
+  mutate(River = "GO",
+         Class = "Catch")
 
 vy_100 <- raster("D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/rasters/s2lc_reprojected.tif") %>% 
   extract(st_read("VY_100mB50.shp")) %>% 
-  lapply(make_table)
+  lapply(make_table) %>% 
+  bind_rows(.id = "column_label")%>% 
+  dplyr::select(-column_label) %>% 
+  mutate(Pixels = rowSums(across(where(is.numeric)), na.rm = TRUE)) %>% 
+  mutate_at(vars(-Pixels), funs(./Pixels)) %>% 
+  bind_cols(sizes_vy %>% dplyr::select(LocCode)) %>% 
+  mutate(River = "VY",
+         Class = "100")
 
 vy_500 <- raster("D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/rasters/s2lc_reprojected.tif") %>% 
   extract(st_read("VY_500mB50.shp")) %>% 
-  lapply(make_table)
+  lapply(make_table) %>% 
+  bind_rows(.id = "column_label")%>% 
+  dplyr::select(-column_label) %>% 
+  mutate(Pixels = rowSums(across(where(is.numeric)), na.rm = TRUE)) %>% 
+  mutate_at(vars(-Pixels), funs(./Pixels)) %>% 
+  bind_cols(sizes_vy %>% dplyr::select(LocCode)) %>% 
+  mutate(River = "VY",
+         Class = "500")
 
 vy_stream <- raster("D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/rasters/s2lc_reprojected.tif") %>% 
   extract(st_read("VY_StreamB50.shp")) %>% 
-  lapply(make_table)
+  lapply(make_table) %>% 
+  bind_rows(.id = "column_label")%>% 
+  dplyr::select(-column_label) %>% 
+  mutate(Pixels = rowSums(across(where(is.numeric)), na.rm = TRUE)) %>% 
+  mutate_at(vars(-Pixels), funs(./Pixels)) %>% 
+  bind_cols(sizes_vy %>% dplyr::select(LocCode)) %>% 
+  mutate(River = "VY",
+         Class = "Stream")
 
 vy_catch <- raster("D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/rasters/s2lc_reprojected.tif") %>% 
   extract(st_read("VY_catch.shp")) %>% 
-  lapply(make_table)
+  lapply(make_table) %>% 
+  bind_rows(.id = "column_label")%>% 
+  dplyr::select(-column_label) %>% 
+  mutate(Pixels = rowSums(across(where(is.numeric)), na.rm = TRUE)) %>% 
+  mutate_at(vars(-Pixels), funs(./Pixels)) %>% 
+  bind_cols(sizes_vy %>% dplyr::select(LocCode)) %>% 
+  mutate(River = "VY",
+         Class = "Catch")
 
-go_100[[1]] %>% 
-  as_factor() %>% 
-  table() %>%
-  as_tibble() %>% 
-  rename(Class = 1, Count = 2) %>% 
-  pivot_wider(names_from = Class, values_from = Count)
-  
 
+# go_100 %>% 
+#   dplyr::select(-column_label) %>% 
+#   mutate(Pixels = rowSums(across(where(is.numeric)), na.rm = TRUE)) %>% 
+#   mutate_at(vars(-Pixels), funs(./Pixels)) %>% 
+#   bind_cols(sizes_go %>% pull(LocCode)) %>% 
+#   mutate(River = "GO",
+#          Class = "100")
