45c6229b787cf31f129d5566bba739bf130d68a8
Simon König <simonkoenig@live.de>
2021-06-29 13:48:58 +0200

    implement extraction for all GO aois (VY not yet available)

--- extract_understory.R
+++ extract_understory.R
@@ -29,6 +29,7 @@ compute_stats <- function(vector){
   return(output)
 }
 
+# GO 100
 go_100 <- st_read("GO_100mB50.shp")
 
 go_100_result <- cover %>% 
@@ -51,3 +52,76 @@ go_100_result <- cover %>%
          Class = "100") %>% 
   dplyr::select(-c(Count_shrub, Count_understory)) %>% 
   rename(Count = Count_cover)
+
+# GO 500
+go_500 <- st_read("go_500mB50.shp")
+
+go_500_result <- cover %>% 
+  extract(go_500) %>% 
+  lapply(compute_stats) %>% 
+  bind_rows() %>% 
+  rename_with(.fn = ~ paste(., "cover", sep = "_")) %>% 
+  bind_cols(shrub %>% 
+              extract(go_500) %>% 
+              lapply(compute_stats) %>% 
+              bind_rows() %>% 
+              rename_with(.fn = ~ paste(., "shrub", sep = "_"))) %>% 
+  bind_cols(understory %>% 
+              extract(go_500) %>% 
+              lapply(compute_stats) %>% 
+              bind_rows() %>% 
+              rename_with(.fn = ~ paste(., "understory", sep = "_"))) %>% 
+  bind_cols(go_500 %>% dplyr::select(LocCode) %>% st_drop_geometry()) %>% 
+  mutate(River = "GO",
+         Class = "500") %>% 
+  dplyr::select(-c(Count_shrub, Count_understory)) %>% 
+  rename(Count = Count_cover)
+
+# GO Stream
+go_stream <- st_read("GO_StreamB50.shp")
+
+go_stream_result <- cover %>% 
+  extract(go_stream) %>% 
+  lapply(compute_stats) %>% 
+  bind_rows() %>% 
+  rename_with(.fn = ~ paste(., "cover", sep = "_")) %>% 
+  bind_cols(shrub %>% 
+              extract(go_stream) %>% 
+              lapply(compute_stats) %>% 
+              bind_rows() %>% 
+              rename_with(.fn = ~ paste(., "shrub", sep = "_"))) %>% 
+  bind_cols(understory %>% 
+              extract(go_stream) %>% 
+              lapply(compute_stats) %>% 
+              bind_rows() %>% 
+              rename_with(.fn = ~ paste(., "understory", sep = "_"))) %>% 
+  bind_cols(go_stream %>% dplyr::select(LocCode) %>% st_drop_geometry()) %>% 
+  mutate(River = "GO",
+         Class = "Stream") %>% 
+  dplyr::select(-c(Count_shrub, Count_understory)) %>% 
+  rename(Count = Count_cover)
+
+# GO Catch
+go_catch <- st_read("GO_catch.shp")
+
+go_catch_result <- cover %>% 
+  extract(go_catch) %>% 
+  lapply(compute_stats) %>% 
+  bind_rows() %>% 
+  rename_with(.fn = ~ paste(., "cover", sep = "_")) %>% 
+  bind_cols(shrub %>% 
+              extract(go_catch) %>% 
+              lapply(compute_stats) %>% 
+              bind_rows() %>% 
+              rename_with(.fn = ~ paste(., "shrub", sep = "_"))) %>% 
+  bind_cols(understory %>% 
+              extract(go_catch) %>% 
+              lapply(compute_stats) %>% 
+              bind_rows() %>% 
+              rename_with(.fn = ~ paste(., "understory", sep = "_"))) %>% 
+  bind_cols(go_catch %>% dplyr::select(LocCode) %>% st_drop_geometry()) %>% 
+  mutate(River = "GO",
+         Class = "Catch") %>% 
+  dplyr::select(-c(Count_shrub, Count_understory)) %>% 
+  rename(Count = Count_cover)
+
