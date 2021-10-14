8d348f6c8eb6c2034c2055a5dba35f57a97f0c04
Simon König <simonkoenig@live.de>
2021-10-14 11:01:22 +0200

    add more columns

--- combine_results.R
+++ combine_results.R
@@ -6,10 +6,10 @@ header <- read_csv("results_biomass.csv") %>%
   select(Index, LocCode, River, Class)
 
 area <- read_csv(
-  "D:/OneDrive/repositories/inverlid/results_01_re.csv") %>%
+  "D:/OneDrive/repositories/inverlid/results_02_re.csv") %>%
   select(Area)
 
-tree <- read_csv("D:/OneDrive/repositories/inverlid/results_01_re.csv") %>% 
+tree <- read_csv("D:/OneDrive/repositories/inverlid/results_02_re.csv") %>% 
   select(Height_max_nodw, Height_ave_nodw, Height_med_nodw, Height_std_nodw,
          Crown_max_nodw, Crown_ave_nodw, Crown_med_nodw, Crown_std_nodw,
          Height_max_conif, Height_ave_conif, Height_med_conif, Height_std_conif,
@@ -17,8 +17,15 @@ tree <- read_csv("D:/OneDrive/repositories/inverlid/results_01_re.csv") %>%
          Height_max_decid, Height_ave_decid, Height_med_decid, Height_std_decid,
          Crown_max_decid, Crown_ave_decid, Crown_med_decid, Crown_std_decid)
 
-composition <- read_csv("D:/OneDrive/repositories/inverlid/results_01_re.csv") %>% 
-  select(Count_full, Count_conif, Count_decid, Count_deadw,)
+composition <- read_csv("D:/OneDrive/repositories/inverlid/results_02_re.csv") %>% 
+  select(Count_full, Count_conif, Count_decid, Count_nodw, Count_deadw, Count_snags,
+         Share_conif, Share_decid, Share_nodw, Share_deadw, Share_snags,
+         Crown_sum_nodw, Crown_sum_conif, Crown_sum_decid,
+         Cover_nodw, Cover_conif, Cover_decid)
+
+biomass <- read_csv("results_biomass.csv") %>% 
+  select(Mean_Biomass, Median_Biomass, Std_Biomass, Max_Biomass,
+         Min_Biomass, Sum_Biomass, Biomass_Area)
 
 # area <- read_csv(
 #   "D:/OneDrive/repositories/inverlid/results_01_re.csv") %>% 
