3d615853ec7d071886fda9cd84fbc83273474a24
Simon König <simonkoenig@live.de>
2022-05-02 15:47:38 +0200

    add comments and clean up

--- code/compute_coverage.R
+++ code/compute_coverage.R
@@ -1,3 +1,21 @@
+# Header --------------------------------------------
+#
+# Original Author: Simon König, simon.koenig@npv-bw.bayern.de 
+# Created on: 2022-04-24
+# 
+# Last modified by: Simon König, simon.koenig@npv-bw.bayern.de 
+# On: 2022-05-02
+#
+# Script Description: estimate coverage
+#
+# Notes: 
+#
+#
+
+
+
+# load libraries and basic data ------------------------------
+
 library(sf)
 library(tidyverse)
 
@@ -7,7 +25,11 @@ all_blocks <- st_read(
 sizes_go <- read_csv("D:/OneDrive/repositories/inverlid/data/sizes_go.csv")
 sizes_vy <- read_csv("D:/OneDrive/repositories/inverlid/data/sizes_vy.csv")
 
-# conif and decid
+
+
+# estimate for conif & decid combined -------------------------
+
+# Große Ohe
 go_100 <- st_read(
   "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/polygons/GO_100mB50.shp") %>% 
   st_intersection(all_blocks %>% filter(TREE_CLASS %in% c("conif", "decid"))) %>% 
@@ -68,8 +90,7 @@ go_results <- bind_rows(go_100, go_500, go_catch, go_stream)
 write_csv(go_results,
           "data/go_results_coverage.csv")
 
-
-
+# Vydra
 vy_100 <- st_read(
   "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/polygons/VY_100mB50.shp") %>% 
   st_intersection(all_blocks %>% filter(TREE_CLASS %in% c("conif", "decid"))) %>% 
@@ -132,13 +153,16 @@ write_csv(vy_results,
           "data/vy_results_coverage.csv")
 
 
-# combine
+# combine results
 results <- bind_rows(go_results, vy_results)
 write_csv(results,
           "data/results_coverage.csv")
 
 
-# only conif
+
+# coniferous only --------------------------------------
+
+# Große Ohe
 go_100_c <- st_read(
   "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/polygons/GO_100mB50.shp") %>% 
   st_intersection(all_blocks %>% filter(TREE_CLASS %in% c("conif"))) %>% 
@@ -199,8 +223,7 @@ go_results_c <- bind_rows(go_100_c, go_500_c, go_catch_c, go_stream_c)
 write_csv(go_results_c,
           "data/go_results_coverage_c.csv")
 
-
-
+# Vydra
 vy_100_c <- st_read(
   "D:/OneDrive/NPBW/Weitere Projekte/InverLid/Data/polygons/VY_100mB50.shp") %>% 
   st_intersection(all_blocks %>% filter(TREE_CLASS %in% c("conif"))) %>% 
@@ -212,7 +235,7 @@ vy_100_c <- st_read(
   st_drop_geometry() %>% 
   as_tibble() %>% 
   select(LocCode, Area, Share) %>% 
-  mutate(River = "GO", Class = "100") %>% 
+  mutate(River = "VY", Class = "100") %>% 
   rename(Area_full = Area, Share_full = Share)
 
 vy_500_c <- st_read(
@@ -226,7 +249,7 @@ vy_500_c <- st_read(
   st_drop_geometry() %>% 
   as_tibble() %>% 
   select(LocCode, Area, Share) %>% 
-  mutate(River = "GO", Class = "500") %>% 
+  mutate(River = "VY", Class = "500") %>% 
   rename(Area_full = Area, Share_full = Share)
 
 vy_catch_c <- st_read(
@@ -240,7 +263,7 @@ vy_catch_c <- st_read(
   st_drop_geometry() %>% 
   as_tibble() %>% 
   select(LocCode, Area, Share) %>% 
-  mutate(River = "GO", Class = "Catch") %>% 
+  mutate(River = "VY", Class = "Catch") %>% 
   rename(Area_full = Area, Share_full = Share)
 
 vy_stream_c <- st_read(
@@ -254,5 +277,16 @@ vy_stream_c <- st_read(
   st_drop_geometry() %>% 
   as_tibble() %>% 
   select(LocCode, Area, Share) %>% 
-  mutate(River = "GO", Class = "Stream") %>% 
+  mutate(River = "VY", Class = "Stream") %>% 
   rename(Area_full = Area, Share_full = Share)
+
+vy_results_c <- bind_rows(vy_100_c, vy_500_c, vy_catch_c, vy_stream_c)# %>% 
+  #mutate(River = "VY")
+write_csv(vy_results_c,
+          "data/vy_results_coverage_c.csv")
+
+
+# combine results
+results <- bind_rows(go_results_c, vy_results_c)
+write_csv(results_c,
+          "data/results_coverage_c.csv")
